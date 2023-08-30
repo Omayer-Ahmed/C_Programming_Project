86dd76c7cc18e6ea1bc9cabff8e2103aca5dfa50
Omayer <ahmedomayer2000@gmail.com>
2023-08-30 20:35:09 +0600

    add new

--- orderfood_by_user.c
+++ orderfood_by_user.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int i;
-for(i=0;i<30;i++)
-printf("%d ",i);
-
-
+int i,n;
+printf("hello");
+printf("\nworld");
+printf("hwllo");
+for(i=0;i<n;i++)
+printf("i");
 }
+ 
